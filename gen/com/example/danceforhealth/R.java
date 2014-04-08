81601d47ce601ff12cb4177f3a2e5d3048d8cc1e
Becky Baumher <bbaumher@gmail.com>
2014-04-07 22:23:30 -0400

    working on previous workouts

--- gen/com/example/danceforhealth/R.java
+++ gen/com/example/danceforhealth/R.java
@@ -24,9 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080005;
+        public static final int linearLayout1=0x7f080003;
         public static final int newWorkout=0x7f080001;
         public static final int preWorkout=0x7f080002;
+        public static final int scrollView1=0x7f080004;
         public static final int stepOneView=0x7f080000;
     }
     public static final class layout {
